3deda97f80d4a765f5a9d9a9956bf0dd4654aa0a
Gerardo Ivan Garcia Gonzalez <gerardo.garcia@ipbtel.local>
2023-07-24 17:33:30 -0600

    Penultima Clase Update: Se hacen adecuaciones para listar projectos.

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Project
+from .models import project
 
-def portfolio(request):
-    projects = Project.objects.all()
-    return render(request, "portfolio/portfolio.html", {'projects': projects})
+def portafolio(request):
+    projects = project.objects.all()
+    return render(request, "portafolio/portafolio.html",
+                  {'projects':projects})  # <=====
+    
